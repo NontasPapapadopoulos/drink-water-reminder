fa53111aaa48618947a2ff589cc48d9e052898ef
Nontas <nodis92@hotmail.com>
2023-01-14 08:49:58 -0800

    add reminder options

---
+++ app/src/main/java/com/nondaspap/drinkwaterreminder/Reminder.kt
@@ -0,0 +1,5 @@
+package com.nondaspap.drinkwaterreminder
+
+class Reminder {
+
+}
--- app/src/main/java/com/nondaspap/drinkwaterreminder/ResultsFragment.kt
+++ app/src/main/java/com/nondaspap/drinkwaterreminder/ResultsFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.TextView
 
 
@@ -15,6 +17,9 @@ class ResultsFragment : Fragment() {
     private lateinit var calculateAgainButton: Button
     private lateinit var sendNotificationsButton: Button
     private lateinit var resultsTextView: TextView
+    private lateinit var sleepTimeSpinner: Spinner
+    private lateinit var wakeUpTimeSpinner: Spinner
+    private lateinit var remindersSpinner: Spinner
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +29,7 @@ class ResultsFragment : Fragment() {
         val view : View = inflater.inflate(R.layout.fragment_results, container, false)
         initComponents(view)
         attatchListeners()
+        setSpinners()
         updateResultMessage()
         return view
     }
@@ -37,12 +43,55 @@ class ResultsFragment : Fragment() {
         this.calculateAgainButton = view.findViewById(R.id.calculateAgainButton)
         this.sendNotificationsButton = view.findViewById(R.id.sendNotificationsButton)
         this.resultsTextView = view.findViewById(R.id.resultsTextView)
+        this.wakeUpTimeSpinner = view.findViewById(R.id.wakeUpTimeSpinner)
+        this.sleepTimeSpinner = view.findViewById(R.id.goToSleepTimeSpinner)
+        this.remindersSpinner = view.findViewById(R.id.remindersSpinner)
+    }
+
+    private fun setSpinners() {
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            activity as MainActivity,
+            R.array.wake_up_time,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            wakeUpTimeSpinner.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(
+            activity as MainActivity,
+            R.array.sleep_time,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            sleepTimeSpinner.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(
+            activity as MainActivity,
+            R.array.reminders,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            remindersSpinner.adapter = adapter
+        }
     }
 
     private fun attatchListeners() {
         calculateAgainButton.setOnClickListener {
             activity?.supportFragmentManager?.popBackStack()
 
+
+        sendNotificationsButton.setOnClickListener {
+
+        }
         }
     }
 
